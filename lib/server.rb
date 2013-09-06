10636a20fb1f0c45a8a183ea3816adc88cdf5f5b
kmazanec <k.mazanec@gmail.com>
2013-09-06 18:19:22 -0500

    added very simple server code

--- lib/server.rb
+++ lib/server.rb
@@ -1,1 +1,12 @@
 # Chat server code goes here
+
+require 'socket'
+
+server = TCPServer.new 2000
+loop do
+  Thread.start(server.accept) do |client|
+    client.puts "Hello !"
+    client.puts "Time is #{Time.now}"
+    client.close
+  end
+end
