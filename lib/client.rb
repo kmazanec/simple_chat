e9ec8bf55c5608b6a03a3bc887b6848c4ba2146f
kmazanec <k.mazanec@gmail.com>
2013-09-06 20:19:04 -0500

    merged jeff's code in

--- lib/client.rb
+++ lib/client.rb
@@ -1,17 +1,56 @@
 # Chat client code goes here
 
-server = TCPSocket.open('localhost',2000)
+require "socket"
 
-
-puts "Connected to server at localhost"
-until server.closed?
-  puts "Say something (close to close): "
-  input = gets.chomp
-  server.puts(input)
-  while line = server.gets
-    puts line.chop
+module Message_Passing
+  $current_message = "blank"
+  def set_current_message(message)
+    $current_message = message
   end
-  break if input == "close"
+
+  def get_current_message
+    $current_message
+  end
 end
 
-server.close
+class TCPSocket
+  include Message_Passing
+end
+
+
+begin
+ clientSession = TCPSocket.new( "localhost", 2008 )
+ 
+rescue StandardError
+  puts "Error !! "
+else
+
+
+  while !(clientSession.closed?)
+
+    print "Enter message :  "
+    msg = gets.chomp
+    clientSession.puts msg
+    puts clientSession.gets
+    # puts clientSession.get_current_message
+  end
+
+end
+
+
+
+# server = TCPSocket.open('localhost',2000)
+
+# puts "Server name: #{server.name}"
+
+# puts "Connected to server at localhost"
+# until server.closed?
+#   puts "Say something (close to close): "
+#   input = gets.chomp
+#   server.puts(input)
+#   line = server.gets
+#   puts line.chop
+#   break if input == "close"
+# end
+
+# server.close
--- lib/server.rb
+++ lib/server.rb
@@ -2,21 +2,75 @@
 
 require 'socket'
 
-server = TCPServer.new 2000
+# server = TCPServer.new 2000
 
-puts "Starting server..."
-loop do
-  Thread.start(server.accept) do |client|
-    input = ""
-    puts "A new client connected!"
-    puts client
-    until input == "close"
-      input = client.gets.chomp
-      puts "Client says: #{input}"
-      client.puts "Server says: Hello!"
-      client.puts "Polly say: #{input}"
-    end
-    client.close
+# puts "Server name: #{server.name}"
+
+
+module Message_Passing
+  $current_message = "empty"
+  def set_current_message(message)
+    $current_message = message
+  end
+
+  def get_current_message
+    $current_message
   end
 end
+
+class TCPServer
+  include Message_Passing
+end
+
+server = TCPServer.new(2008)
+
+# connection = server.accept
+loop do
+  Thread.start(server.accept) do |connection|
+# connection.puts "HELLO"
+    name = nil
+    while (input = connection.gets.chomp)
+      puts input
+
+      # set_current_message(input)
+
+      input_array = input.split(" ")
+      word1 = input_array[0]
+      word2 = input_array[1]
+      word3 = input_array[2]
+      word4 = input_array[3]
+
+      if input == "What time is it?"
+        connection.puts "The current local time is #{Time.now.to_s}"
+      elsif word1 == "My" && word2 == "name" && word3 == "is"
+        name = word4
+        connection.puts "Ok, I'll call you #{name} from now on!"
+      elsif !name.nil?
+        connection.puts "I can't let you do that, #{name}."
+      else
+        connection.puts "I can't let you do that, Dave."
+      end
+    end
+  end
+end
+
+
+
+
+# puts "Starting server..."
+# loop do
+#   Thread.start(server.accept) do |client|
+#     input = ""
+#     puts "A new client connected!"
+#     puts client
+
+#     while (input = client.gets.chomp) && (input != "close")
+#       puts "Client says: #{input}"
+#       client.puts "Server says: Hello!"
+#       client.puts "Polly say: #{input}"
+#     end
+#     client.close
+#     puts "Client disconnected"
+#   end
+# end
 
